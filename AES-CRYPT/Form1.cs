199447187ee110f70fda438dca8989a71088f03f
Kirill S. <kirr2@gf.tom.ru>
2015-10-16 15:45:44 +0600

    Добавлен класс aes

--- AES-CRYPT/Form1.cs
+++ AES-CRYPT/Form1.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
+using System.IO;
+using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace AES_CRYPT
@@ -13,6 +9,7 @@ namespace AES_CRYPT
     public partial class Form1 : Form
     {
         bool crpt = true;
+        string ans = "";
 
         public Form1()
         {
@@ -25,7 +22,19 @@ namespace AES_CRYPT
             {
                 if (textBox2.TextLength != 0)
                 {
-                    label1.Text = "Шифруем...";
+                    if (crpt)
+                    {
+                        label1.Text = "Шифруем...";
+                        ans = aes.Encrypt(textBox1.Text, textBox2.Text, "VK", "SHA1", 2, "SCHOOL2283221488", 256);
+                        textBox3.Text = ans;
+                    }
+                    else
+                    {
+                        label1.Text = "Дешифруем...";
+                        ans = aes.Decrypt(textBox1.Text, textBox2.Text, "VK", "SHA1", 2, "SCHOOL2283221488", 256);
+                        textBox3.Text = ans;
+                    }
+
                 }
                 else label1.Text = "Необходимо ввести пароль!";
             }
@@ -46,6 +55,84 @@ namespace AES_CRYPT
                 label1.Text = "Введите текст и пароль для шифрования";
                 button1.Text = "Encrypt";
             }
+        }
+    }
+    public class aes
+    {
+        public static string Encrypt(string plainText, string password, string salt = "salt", string hashAlgorithm = "SHA1", int passwordIterations = 2, string initialVector = "OFRna73m*aze01xY", int keySize = 256)
+        {
+            if (string.IsNullOrEmpty(plainText))
+                return "";
+
+            byte[] initialVectorBytes = Encoding.ASCII.GetBytes(initialVector);
+            byte[] saltValueBytes = Encoding.ASCII.GetBytes(salt);
+            byte[] plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+
+            PasswordDeriveBytes derivedPassword = new PasswordDeriveBytes
+             (password, saltValueBytes, hashAlgorithm, passwordIterations);
+
+            byte[] keyBytes = derivedPassword.GetBytes(keySize / 8);
+            RijndaelManaged symmetricKey = new RijndaelManaged();
+            symmetricKey.Mode = CipherMode.CBC;
+
+            byte[] cipherTextBytes = null;
+
+            using (ICryptoTransform encryptor = symmetricKey.CreateEncryptor
+            (keyBytes, initialVectorBytes))
+            {
+                using (MemoryStream memStream = new MemoryStream())
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream
+                             (memStream, encryptor, CryptoStreamMode.Write))
+                    {
+                        cryptoStream.Write(plainTextBytes, 0, plainTextBytes.Length);
+                        cryptoStream.FlushFinalBlock();
+                        cipherTextBytes = memStream.ToArray();
+                        memStream.Close();
+                        cryptoStream.Close();
+                    }
+                }
+            }
+
+            symmetricKey.Clear();
+            return Convert.ToBase64String(cipherTextBytes);
+        }
+        public static string Decrypt(string cipherText, string password, string salt = "salt", string hashAlgorithm = "SHA1", int passwordIterations = 2, string initialVector = "OFRna73m*aze01xY", int keySize = 256)
+        {
+            if (string.IsNullOrEmpty(cipherText))
+                return "";
+
+            byte[] initialVectorBytes = Encoding.ASCII.GetBytes(initialVector);
+            byte[] saltValueBytes = Encoding.ASCII.GetBytes(salt);
+            byte[] cipherTextBytes = Convert.FromBase64String(cipherText);
+
+            PasswordDeriveBytes derivedPassword = new PasswordDeriveBytes
+            (password, saltValueBytes, hashAlgorithm, passwordIterations);
+            byte[] keyBytes = derivedPassword.GetBytes(keySize / 8);
+
+            RijndaelManaged symmetricKey = new RijndaelManaged();
+            symmetricKey.Mode = CipherMode.CBC;
+
+            byte[] plainTextBytes = new byte[cipherTextBytes.Length];
+            int byteCount = 0;
+
+            using (ICryptoTransform decryptor = symmetricKey.CreateDecryptor
+                     (keyBytes, initialVectorBytes))
+            {
+                using (MemoryStream memStream = new MemoryStream(cipherTextBytes))
+                {
+                    using (CryptoStream cryptoStream
+                    = new CryptoStream(memStream, decryptor, CryptoStreamMode.Read))
+                    {
+                        byteCount = cryptoStream.Read(plainTextBytes, 0, plainTextBytes.Length);
+                        memStream.Close();
+                        cryptoStream.Close();
+                    }
+                }
+            }
+
+            symmetricKey.Clear();
+            return Encoding.UTF8.GetString(plainTextBytes, 0, byteCount);
         }
     }
 }
